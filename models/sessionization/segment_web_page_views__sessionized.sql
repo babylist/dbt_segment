1af99a0b3f02c5002fb9cbb37e72ff9e06623a4d
arthurnw <arthur.wang@babylist.com>
2024-04-17 13:15:41 -0700

    Fix typo in surrogate key

--- models/sessionization/segment_web_page_views__sessionized.sql
+++ models/sessionization/segment_web_page_views__sessionized.sql
@@ -147,7 +147,7 @@ session_ids as (
 
         {{dbt_utils.star(ref('segment_web_page_views'))}},
         page_view_number,
-        {{dbt_utils.surrogate_key(['anonymous_id', 'session_start_testamp', 'session_number'])}} as session_id
+        {{dbt_utils.surrogate_key(['anonymous_id', 'session_start_tstamp', 'session_number'])}} as session_id
 
     from session_starts
 
